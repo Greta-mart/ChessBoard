8af84b01f8b3410db658d8b15f5bbf3a61cbca8b
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-04-30 20:40:02 +0300

    chessBoard

---
+++ src/ChessBoard.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ChessBoard {
+    int length;
+    int width;
+
+    ChessBoard(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public void chessBoardDrawing() {
+        for (int i = 0; i < this.length; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < this.width; j++) {
+                    System.out.print("*");
+                    System.out.print(" ");
+                }
+            } else {
+                System.out.println();
+                for (int j = 0; j < this.width; j++) {
+                    System.out.print(" ");
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,27 @@
 import static java.lang.Integer.parseInt;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
-        chessBoard board = new chessBoard();
-
         int length = 0;
         int width = 0;
-        if (args.length > 0)
+
+        if (args.length == 2)
             try {
-                width = Integer.parseInt(args[0]);
-                length = Integer.parseInt(args[1]);
+                length = Integer.parseInt(args[0]);
+                width = Integer.parseInt(args[1]);
             } catch (NumberFormatException e) {
                 System.err.println("Argument" + " must be an integer");
-                System.exit(1);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.err.println("The width and length should be more than 0");
                 System.exit(1);
             }
         else {
             System.out.println("Please insert width and length after name of class");
+            System.exit(1);
         }
-        board.chessBoard(length, width);
+
+        if (length > 0 && width > 0) {
+            ChessBoard board = new ChessBoard(length, width);
+            board.chessBoardDrawing();
+        } else
+            System.out.println("The width and length should be greater than 0");
     }
 }
