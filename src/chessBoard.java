61ba42185d49409b18e69e9cd71252819ae02a92
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-04-30 14:18:10 +0300

    chessboard

---
+++ src/chessBoard.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class chessBoard {
+
+    int lenght;
+    int width;
+
+    public void chessBoard(int lenght, int width) {
+        for (int i = 0; i < width; i++)
+        {
+            if (i % 2 == 0) {
+                for (int j = 0; j < lenght; j++)
+                    System.out.print("* ");
+            } else {
+                System.out.println();
+                for (int j = 0; j < lenght; j++)
+                    System.out.print(" *");
+                System.out.println();
+            }
+        }
+    }
+}
