e81d622c668ae1cd9939cc3cdca38d721a53daf4
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-04-30 21:26:13 +0300

    ChessBoard

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -12,13 +12,17 @@ public class ChessBoard {
             if (i % 2 == 0) {
                 for (int j = 0; j < this.width; j++) {
                     System.out.print("*");
-                    System.out.print(" ");
+                    {
+                        System.out.print(" ");
+                    }
                 }
             } else {
                 System.out.println();
                 for (int j = 0; j < this.width; j++) {
                     System.out.print(" ");
-                    System.out.print("*");
+                    {
+                        System.out.print("*");
+                    }
                 }
                 System.out.println();
             }
